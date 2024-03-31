4be07667d94aad3334a820c20c41bf8949f58a9d
tim <batr.tim@gmail.com>
2024-03-31 22:05:02 +0300

    create server

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
-let msg = "hello world"
+const { createServer } = require('http')
 
-console.log(msg)
+const hostname = '127.0.0.1'
+const port = 3000
+
+const server = createServer((req, res) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('hello world')
+})
+
+server.listen(port, hostname, () => {
+    console.log(`Server listen on ${port} port`)
+})
